e1ba869d724d4f96d14a9d56c0da8822b651d905
MuhammadA15 <47173221+MuhammadA15@users.noreply.github.com>
2019-03-12 18:50:19 -0500

    Some Routes Added

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,44 @@
 import React, { Component } from 'react';
+import { Switch, Route, Link, BrowserRouter } from 'react-router-dom';
 import './App.css';
-import Login from './components/login/Login'
+import Login from './components/login/Login';
+import SignUpPage from './components/sign_up/SignUp';
 
 // Main component
+
+
+const Main = () => (
+  <main>
+    <Switch>
+      <Route exact path='/SignUp' component={SignUpPage}/>
+      <Route exact path='/' component={Login}/>
+    </Switch>
+  </main>
+)
+
+const Header = () => (
+  <header style={{textAlign: "left"}}>
+    <nav>
+      <ul>
+        <li><Link to='/SignUp'>Sign Up</Link></li>
+        <li><Link to='/'>Login</Link></li>
+      </ul>
+    </nav>
+  </header>
+)
+
 class App extends Component {
   render() {
     return (
+      <BrowserRouter>
       <div className="App"> 
-       <Login />  
+        <Header />
+        <Main />
       </div>
+      </BrowserRouter>
     );
   }
 }
+
 
 export default App;
