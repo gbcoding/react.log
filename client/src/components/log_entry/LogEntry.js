098227ee1952d4bc1df540c5205128bb49fcb95e
Alex <esjai.hk@gmail.com>
2019-04-13 13:34:21 -0500

    Log View Styling Update

--- client/src/components/log_entry/LogEntry.js
+++ client/src/components/log_entry/LogEntry.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { Text, View } from 'react-native';
+import { Button, Row, Col } from 'reactstrap';
 import './LogEntry.css';
+
+import flag_true from '../../images/flag_true.png';
+import flag_false from '../../images/flag_false.png';
 
 export class LogEntry extends Component {
     
@@ -8,17 +12,102 @@ export class LogEntry extends Component {
         super(props);
 
         this.state = {
-            items: []
+            error: null,
+            show: false
         };
+    }
+
+    showToggle = () => {
+        const { show } = this.state;
+        this.setState( { show: !show } );
     }
 
 
     render(){
         const item = this.props.item;
+    
+        return(
+            <View style={{ flexDirection: 'row', alignSelf: 'stretch'}}>
+                <div className="item" key={item.log_id}>
+                    <Row>
+                        <Col xs="3">
+                            <Text  style={{fontSize: 20}} adjustsFontSizeToFit minimumFontScale={.5} numberOfLines={1} allowFontScaling> 
+                                {item.food_consumed}
+                            </Text>
+                        </Col>
+                        <Col xs="2">
+                            <img src={item.issue_flag=="1" ? flag_true : flag_false}/>
+                        </Col>
+                        <Col xs="2">
+                            <Text  style={{fontSize: 20}} adjustsFontSizeToFit minimumFontScale={.5} numberOfLines={1} allowFontScaling> 
+                                {item.date.substring(0,10)}
+                            </Text>
+                        </Col>
+                        <Col xs="2">
+                            <Text  style={{fontSize: 20}} adjustsFontSizeToFit minimumFontScale={.5} numberOfLines={1} allowFontScaling> 
+                                {item.time}
+                            </Text>  
+                        </Col>
+                        <Col  xs="2">
+                            <Button color="primary" size="sm" onClick={ this.showToggle } block>
+                                {this.state.show ? "Hide Details" : "Show Details"}
+                            </Button>
+                        </Col>
+                    </Row>
+
+                    { this.state.show && <LogDetail item={item} />}
+                   
+                    
+                </div>
+            </View>
+        );
+    }
+}
+
+class LogDetail extends Component {
+    
+    constructor(props) {
+        super(props);
+
+    }
+
+    render(){
+        const item = this.props.item;
 
         return(
-            <View style={{ flexDirection: 'row', height: 100}}>
-                <div className="item" key={item.log_id}>
+            
+            <div className="detail">
+                <hr />
+                    <Row>
+                        <Col>
+                            <Text style={{fontSize: 20}} adjustsFontSizeToFit minimumFontScale={.5} numberOfLines={1} allowFontScaling> 
+                                Type: {item.meal_type}
+                            </Text>
+                        </Col>
+                        <Col>
+                            <Text style={{fontSize: 20}} adjustsFontSizeToFit minimumFontScale={.5} numberOfLines={1} allowFontScaling> 
+                                Duration: {item.duration} 
+                            </Text>
+                        </Col> 
+                        <Col>
+                            <Text style={{fontSize: 20}} adjustsFontSizeToFit minimumFontScale={.5} numberOfLines={1} allowFontScaling>
+                                Severity: {item.severity}
+                            </Text>
+                        </Col> 
+                    </Row>
+                    <Row>
+                        <Col>
+                            <Text style={{fontSize: 20}} adjustsFontSizeToFit minimumFontScale={.5} allowFontScaling>
+                                Note: {item.notes}
+                            </Text>
+                        </Col>
+                    </Row>
+            
+            </div>
+           
+
+
+            /*
                     <div className="firstline">
                         <span> Date: {item.date} Time: {item.time} </span>
                     </div>
@@ -31,8 +120,7 @@ export class LogEntry extends Component {
                     <div className="forthline">
                         <span>Note: {item.notes} </span>
                     </div>
-                </div>
-            </View>
+                    */
         );
     }
 }
